6f8941c2aed732e19c7f78d650b762e646508934
qiuxia <397401374@qq.com>
2018-05-03 09:58:07 +0800

    conic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## Demo
+## 文档参考
+
+- [张鑫旭，获取元素CSS值之getComputedStyle方法熟悉](http://www.zhangxinxu.com/wordpress/2012/05/getcomputedstyle-js-getpropertyvalue-currentstyle/)
+
+- http://jdc.jd.com/archives/212063
--- index.js
+++ index.js
@@ -19,19 +19,29 @@ window.onload = function() {
 //   outputElement.setAttribute('for', rangeInputElement.id);
   updateRangeValue();
 
+  // 判断浏览器是否支持 conic-gradient() ，支持的话增加 .full
+  console.log('getComputedStyle:', getComputedStyle(outputElement).length)
+  console.log('style:', outputElement.style.length)
+  if (getComputedStyle(outputElement).backgroundImage !== 'none') {
+    outputElement.classList.add('support-conic-gradient')
+  }
+
   // 滑块滚动时监听值的变化
   wrapperElement.addEventListener('input', updateRangeValue, false)
   wrapperElement.addEventListener('change', updateRangeValue, false)
 
+  // 实时更新滑块的值
   function updateRangeValue() {
     let trackWidth = 275
     let thumbDiameter = 0.1 * trackWidth
 
     if (rangeValue !== rangeInputElement.value) {
       outputElement.value = rangeValue = rangeInputElement.value
-      outputElement.style.transform = 'translate(' + rangeValue / 100 * (trackWidth - thumbDiameter) + 'px, 0)'
+
+      // 跟着滑块移动
+      // outputElement.style.transform = 'translate(' + rangeValue / 100 * (trackWidth - thumbDiameter) + 'px, 0)'
       // 在css中使用var()，绑定在容器上的css变量
-      // wrapperElement.style.setProperty('--val', rangeValue)
+      wrapperElement.style.setProperty('--val', rangeValue)
       // console.log('wrapperElement:', wrapperElement)
     }
   }
